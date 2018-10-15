15f443f4a49449fd5bf7cae0b95418303471100e
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-10-16 01:12:16 +0200

    First implementation of delete button

--- src/pl/edu/agh/iisg/to/javafx/cw1/controller/AccountOverviewController.java
+++ src/pl/edu/agh/iisg/to/javafx/cw1/controller/AccountOverviewController.java
@@ -1,6 +1,9 @@
 package pl.edu.agh.iisg.to.javafx.cw1.controller;
 
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import pl.edu.agh.iisg.to.javafx.cw1.model.*;
@@ -28,11 +31,21 @@ public class AccountOverviewController {
     private TableColumn<Transaction, BigInteger> inflowColumn;
 
     @FXML
+    private Button deleteButton;
+
+    @FXML
+    private void handleDeleteAction(ActionEvent event) {
+        data.getTransactions().removeAll(transactionsTable.getSelectionModel().getSelectedItems());
+    }
+
+    @FXML
     private void initialize(){
         dateColumn.setCellValueFactory(dataValue -> dataValue.getValue().getDateProperty());
         payeeColumn.setCellValueFactory(dataValue -> dataValue.getValue().getPayeeProperty());
         categoryColumn.setCellValueFactory(dataValue -> dataValue.getValue().getCategoryProperty());
         inflowColumn.setCellValueFactory(dataValue -> dataValue.getValue().getInflowProperty());
+
+        deleteButton.disableProperty().bind(Bindings.isEmpty(transactionsTable.getSelectionModel().getSelectedItems()));
     }
 
     public void setData(Account account) {
